3f032d019b112bfa3102473e848cb83a66e6504e
Hugo Daniel <hugodan4747@gmail.com>
2018-11-07 15:20:37 -0500

    uptate git

--- UserInterfaceTkinter_Ph3.py
+++ UserInterfaceTkinter_Ph3.py
@@ -11,7 +11,7 @@ import tkinter.ttk as ttk
 import cv2
 import AeroGarden
 import Vision
-import ToolChanger
+#import ToolChanger
 import warnings
 import serial
 import serial.tools.list_ports
@@ -20,7 +20,7 @@ from time import sleep
 # Global def
 Tresh_Area = 100
 Dist_Tresh = 150
-My_Path = "/home/pi/Documents/GitRepo"
+My_Path = "/Users/hugodaniel/Desktop/StageMedellin/GitRepo/Image"
 
 
 def setUpArduino():
@@ -54,8 +54,8 @@ class CurrentPosition:
         self.input_CNC.bind('<Return>',self.updateCNCPosition)
 
     def updateCNCPosition(self, event):
-        Tool.current_tool.setGripperPosition(self.input_CNC.get())
-        #self.CNC_current_position.configure(str(self.input_CNC.get()))
+        #Tool.current_tool.setGripperPosition(self.input_CNC.get())
+        self.CNC_current_position.configure(text=3000)#str(self.input_CNC.get()))
 
 class Capture:
     # Capture
@@ -96,7 +96,7 @@ class Capture:
 
 class CurrentTool:
     # Current tool
-    def __init__(self, frame, Gripper, Ultra_Sonic_Sensor, EC_Sensor, Temperature_Sensor, Ph_Sensor):        
+    def __init__(self, frame, Gripper, Ultrasonic_Sensor, EC_Sensor, Temperature_Sensor, Ph_Sensor):        
         self.text_tool= tk.Label(frame, text="Current tool")
         self.text_tool.grid(column=0, row=0)
         
@@ -105,7 +105,7 @@ class CurrentTool:
         self.combo.current(0) #set the selected item
         self.combo.grid(column=1, row=0)
         self.gripper = Gripper
-        self.ultra_sonic_sensor = Ultra_Sonic_Sensor
+        self.ultrasonic_sensor = Ultrasonic_Sensor
         self.ec_sensor = EC_Sensor
         self.temperature_sensor = Temperature_Sensor
         self.ph_sensor = Ph_Sensor
@@ -116,43 +116,52 @@ class CurrentTool:
 
     def updateTool(self, event):
         #update ToolChanger
-        Tool.setCurrentTool("None")
+        #Tool.setCurrentTool("None")
         
         self.selected_tool = self.combo.get()
         self.gripper.button_gripper_open.configure(state=tk.DISABLED)
         self.gripper.button_gripper_close.configure(state=tk.DISABLED)
         self.gripper.gripper_state.configure(text="--")
-        self.ultra_sonic_sensor.distance_untrasonic_sensor.grid_remove()
-        self.ec_sensor.EC_EC_sensor.grid_remove()
-        self.temperature_sensor.temp_temperature_sensor.grid_remove()
-        self.ph_sensor.Ph_Ph_sensor.grid_remove()
+        self.ultrasonic_sensor.active = False
+        self.ultrasonic_sensor.updateUltrasonicSensor()
+        self.ec_sensor.active = False
+        self.ec_sensor.updateECSensor()
+        self.temperature_sensor.active = False
+        self.temperature_sensor.updateTemperatureSensor()
+        self.ph_sensor.active = False
+        self.ph_sensor.updatePhSensor() 
         
         self.gripper.blurGripperImage()
+        
         if self.selected_tool == "Gripper":
-            Tool.setCurrentTool("Gripper")
+            #Tool.setCurrentTool("Gripper")
             
             self.gripper.gripper_state.configure(text="Open")
             self.gripper.openGripperImage()
             self.gripper.button_gripper_open.configure(state=tk.NORMAL)
             self.gripper.button_gripper_close.configure(state=tk.NORMAL)
         elif self.selected_tool == "Ultrasonic sensor":
-            Tool.setCurrentTool("Ultrasonic sensor")
-            
-            self.ultra_sonic_sensor.distance_untrasonic_sensor.grid()
+            #Tool.setCurrentTool("Ultrasonic sensor")
+            self.ultrasonic_sensor.active = True
+            self.ultrasonic_sensor.updateUltrasonicSensor()
+            #self.ultrasonic_sensor.distance_untrasonic_sensor.grid()
         elif self.selected_tool == "EC and temperature sensor":
-            Tool.setCurrentTool("ECTempSensor")
+            #Tool.setCurrentTool("ECTempSensor")
             
-            self.ec_sensor.EC_EC_sensor.grid()
-            self.temperature_sensor.temp_temperature_sensor.grid()
+            self.ec_sensor.active = True
+            self.ec_sensor.updateECSensor()
+            self.temperature_sensor.active = True
+            self.temperature_sensor.updateTemperatureSensor()
         elif self.selected_tool == "Ph sensor":
-            Tool.setCurrentTool("PhSensor")
+            #Tool.setCurrentTool("PhSensor")
             
-            self.ph_sensor.Ph_Ph_sensor.grid()
+            self.ph_sensor.active = True
+            self.ph_sensor.updatePhSensor() 
     
 class Gripper:
     # Gripper
-    def __init__(self, frame, frame_tool):
-        self.frame_tool = frame_tool
+    def __init__(self, frame, frame_photo):
+        self.frame_photo = frame_photo
         self.text_gripper = tk.Label(frame, text="State: ")
         self.text_gripper.grid(column=0, row =0)
         self.gripper_state = tk.Label(frame, text="--")
@@ -163,18 +172,18 @@ class Gripper:
         self.button_gripper_close.grid(column=1, row =1)
         
         #Init gripper image
-        self.canvas = tk.Canvas(self.frame_tool, width=100, height=100, state=tk.DISABLED)
+        self.canvas = tk.Canvas(self.frame_photo, width=100, height=100, state=tk.DISABLED)
         self.canvas.pack(fill="both", expand=True)
         self.blurGripperImage()
 
     def openGripper(self):
-        Tool.current_tool.setGripperPosition(1900)
+        #Tool.current_tool.setGripperPosition(1900)
         
         self.gripper_state.configure(text="Open")
         self.openGripperImage()
         
     def closeGripper(self):
-        Tool.current_tool.setGripperPosition(1400)
+        #Tool.current_tool.setGripperPosition(1400)
         
         self.gripper_state.configure(text="Close")
         self.closeGripperImage()
@@ -197,63 +206,149 @@ class Gripper:
         self.canvas.create_image(50,50,image=self.capture)
         self.canvas.image = self.capture
 
-class UltraSonicSensor:
+class UltrasonicSensor:
     # Ultrasonic sensor
-    def __init__(self, frame):
+    def __init__(self, frame, frame_photo):
+        self.frame_photo = frame_photo
+        self.frame = frame
+        self.active = False
         self.text_ultrasonic_sensor = tk.Label(frame, text="Distance: ")
         self.text_ultrasonic_sensor.grid(column=0, row =0)
         self.distance_untrasonic_sensor = tk.Label(frame)
         self.distance_untrasonic_sensor.grid(column=1, row =0)
-        frame.after(500, self.updateUltrasonicSensor)
-        self.distance_untrasonic_sensor.grid_remove()
+        self.distance_untrasonic_sensor.configure(text="--")
+        #self.frame.after(500, self.updateUltrasonicSensor)
+        #self.distance_untrasonic_sensor.grid_remove()
+        
+        #Init ultrasonic sensor image
+        self.canvas = tk.Canvas(self.frame_photo, width=100, height=100, state=tk.DISABLED)
+        self.canvas.pack(fill="both", expand=True)
+        self.blurUltrasonicSensorImage()
 
     def updateUltrasonicSensor(self):
-        self.distance_untrasonic_sensor.configure(text=40)
+        if self.active == True:            
+            self.distance_untrasonic_sensor.configure(text=40)
+            self.frame.after(500, lambda: self.updateUltrasonicSensor())
+            self.clearUltrasonicSensorImage()
+        else:
+            self.distance_untrasonic_sensor.configure(text="--")
+            self.blurUltrasonicSensorImage()
+
+    def clearUltrasonicSensorImage(self):
+        Vision.resizeImage(My_Path+"/UltrasonicSensorClear.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/UltrasonicSensorClear.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
+        
+    def blurUltrasonicSensorImage(self):
+        Vision.resizeImage(My_Path+"/UltrasonicSensorBlur.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/UltrasonicSensorBlur.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
         
 class ECSensor:
     # Ultrasonic sensor
-    def __init__(self, frame):
+    def __init__(self, frame, frame_photo):
+        self.frame_photo = frame_photo
+        self.frame = frame
+        self.active = False
         self.text_EC_EC_sensor = tk.Label(frame, text="EC (S/cm): ")
         self.text_EC_EC_sensor.grid(column=0, row =0)
         self.EC_EC_sensor = tk.Label(frame)
         self.EC_EC_sensor.grid(column=1, row =0)
-        frame.after(500, self.updateECSensor)
-        self.EC_EC_sensor.grid_remove()
+        self.frame.after(500, self.updateECSensor)
+        self.EC_EC_sensor.configure(text="--")
+        
+        #Init EC sensor image
+        self.canvas = tk.Canvas(self.frame_photo, width=100, height=100, state=tk.DISABLED)
+        self.canvas.pack(fill="both", expand=True)
+        self.blurECSensorImage()
 
     def updateECSensor(self):
-        self.EC_EC_sensor.configure(text=40)
+        if self.active == True:            
+            self.EC_EC_sensor.configure(text=40)
+            self.frame.after(500, lambda: self.updateECSensor())
+            self.clearECSensorImage()
+        else:
+            self.EC_EC_sensor.configure(text="--")
+            self.blurECSensorImage()
+        
+    def clearECSensorImage(self):
+        Vision.resizeImage(My_Path+"/ECSensorClear.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/ECSensorClear.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
+        
+    def blurECSensorImage(self):
+        Vision.resizeImage(My_Path+"/ECSensorBlur.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/ECSensorBlur.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
         
 class TemperatureSensor:
     # Ultrasonic sensor
-    def __init__(self, frame):
+    def __init__(self, frame, frame_photo):
+        self.frame_photo = frame_photo
+        self.frame = frame
+        self.active = False
         self.text_temperature_sensor= tk.Label(frame, text="Temperature (°C): ")
         self.text_temperature_sensor.grid(column=0, row =0)
         self.temp_temperature_sensor = tk.Label(frame)
         self.temp_temperature_sensor.grid(column=1, row =0)
-        frame.after(500, self.updateTemperatureSensor)
-        self.temp_temperature_sensor.grid_remove()
+        self.frame.after(500, self.updateTemperatureSensor)
+        self.temp_temperature_sensor.configure(text="--")
 
-    def updateTemperatureSensor(self):
-        self.temp_temperature_sensor.configure(text=40)
+    def updateTemperatureSensor(self): 
+        if self.active == True:            
+            self.temp_temperature_sensor.configure(text=40)
+            self.frame.after(500, lambda: self.updateTemperatureSensor())
+        else:
+            self.temp_temperature_sensor.configure(text="--")
         
 class PhSensor:
     # Ultrasonic sensor
-    def __init__(self, frame):
+    def __init__(self, frame, frame_photo):
+        self.frame_photo = frame_photo
+        self.frame = frame
+        self.active = False
         self.text_Ph_sensor = tk.Label(frame, text="Ph: ")
         self.text_Ph_sensor.grid(column=0, row =0)
         self.Ph_Ph_sensor = tk.Label(frame)
         self.Ph_Ph_sensor.grid(column=1, row =0)
-        frame.after(500, self.updatePhSensor)
-        self.Ph_Ph_sensor.grid_remove()
+        self.frame.after(500, self.updatePhSensor)
+        self.Ph_Ph_sensor.configure(text="--")
+        
+        #Init EC sensor image
+        self.canvas = tk.Canvas(self.frame_photo, width=100, height=100, state=tk.DISABLED)
+        self.canvas.pack(fill="both", expand=True)
+        self.blurPhSensorImage()
 
     def updatePhSensor(self):
-        self.Ph_Ph_sensor.configure(text=40)
+        if self.active == True:            
+            self.Ph_Ph_sensor.configure(text=40)
+            self.frame.after(500, lambda: self.updatePhSensor())
+            self.clearPhSensorImage()
+        else:
+            self.Ph_Ph_sensor.configure(text="--")
+            self.blurPhSensorImage()
+        
+    def clearPhSensorImage(self):
+        Vision.resizeImage(My_Path+"/PhSensorClear.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/PhSensorClear.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
+        
+    def blurPhSensorImage(self):
+        Vision.resizeImage(My_Path+"/PhSensorBlur.png", 100)
+        self.capture = tk.PhotoImage(file=(My_Path+"/PhSensorBlur.png"))
+        self.canvas.create_image(50,50,image=self.capture)
+        self.canvas.image = self.capture
 
 class MainApplication:
     def __init__(self, master):
         # Window init
         master.title("AeroponicBot GUI")
-        master.geometry('650x450')
+        master.geometry('600x650')
 
         # Frames
         #Current position
@@ -269,11 +364,12 @@ class MainApplication:
         frame_gripper = tk.LabelFrame(frame_current_tool, text="Gripper")
         frame_gripper.grid(column=0, row=3, padx=5, pady=2)
         frame_photo_gripper = tk.Frame(frame_current_tool)
-        frame_photo_gripper.grid(column=1, row=3, padx=5, pady=5)
+        frame_photo_gripper.grid(column=1, row=3, padx=2, pady=2)
         #Ultrasonic sensor
-        frame_ultra_sonic_sensor = tk.LabelFrame(frame_current_tool, text="Ultrasonic sensor")
-        frame_ultra_sonic_sensor.grid(column=0, row=4, padx=2, pady=2)
-        
+        frame_ultrasonic_sensor = tk.LabelFrame(frame_current_tool, text="Ultrasonic sensor")
+        frame_ultrasonic_sensor.grid(column=0, row=4, padx=2, pady=2)
+        frame_photo_ultrasonic = tk.Frame(frame_current_tool)
+        frame_photo_ultrasonic.grid(column=1, row=4, padx=2, pady=2)        
         #EC and temperature sensor
         frame_EC_Temp_sensor = tk.LabelFrame(frame_current_tool, text="EC and temperature sensor")
         frame_EC_Temp_sensor.grid(column=0, row=5, padx=2, pady=2)
@@ -283,20 +379,25 @@ class MainApplication:
         #Temperature sensor
         frame_temperature_sensor = tk.Frame(frame_EC_Temp_sensor)
         frame_temperature_sensor.grid(column=0, row=1, padx=2, pady=2)
+        #Photo EC and temperature sensor
+        frame_photo_EC_temp = tk.Frame(frame_current_tool)
+        frame_photo_EC_temp.grid(column=1, row=5, padx=2, pady=2)
         #Ph sensor
         frame_Ph_sensor = tk.LabelFrame(frame_current_tool, text="Ph sensor")
         frame_Ph_sensor.grid(column=0, row=6, padx=2, pady=2)
+        frame_photo_Ph = tk.Frame(frame_current_tool)
+        frame_photo_Ph.grid(column=1, row=6, padx=2, pady=2)
         
         
         # Setup Widgets
         self.current_position = CurrentPosition(frame_current_position)
         self.capture = Capture(frame_capture)
         self.gripper = Gripper(frame_gripper, frame_photo_gripper)
-        self.ultra_sonic_sensor = UltraSonicSensor(frame_ultra_sonic_sensor)
-        self.ec_sensor = ECSensor(frame_EC_sensor)
-        self.temperature_sensor = TemperatureSensor(frame_temperature_sensor)
-        self.ph_sensor = PhSensor(frame_Ph_sensor)
-        self.current_tool = CurrentTool(frame_current_tool, self.gripper, self.ultra_sonic_sensor, self.ec_sensor, self.temperature_sensor, self.ph_sensor)
+        self.ultrasonic_sensor = UltrasonicSensor(frame_ultrasonic_sensor, frame_photo_ultrasonic)
+        self.ec_sensor = ECSensor(frame_EC_sensor, frame_photo_EC_temp)
+        self.temperature_sensor = TemperatureSensor(frame_temperature_sensor, frame_photo_EC_temp)
+        self.ph_sensor = PhSensor(frame_Ph_sensor, frame_photo_Ph)
+        self.current_tool = CurrentTool(frame_current_tool, self.gripper, self.ultrasonic_sensor, self.ec_sensor, self.temperature_sensor, self.ph_sensor)
         
 
 
@@ -309,8 +410,8 @@ if __name__ == "__main__":
     #    ser = serial.Serial(arduino_ports[0], 9600, 8, 'N', 1, timeout=1)
     
     #Inint tool changer
-    Tool = ToolChanger.Tool("None")
-    Tool.setCurrentTool("Gripper")
+    #Tool = ToolChanger.Tool("None")
+    #Tool.setCurrentTool("Gripper")
     #Init Garden
     G = AeroGarden.Garden()
     #setUpGarden(G)
