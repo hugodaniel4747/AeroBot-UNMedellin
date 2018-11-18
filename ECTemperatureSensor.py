1a9ec8831f3934fbcdd8f5a5a790efa2be78b1fb
Hugo Daniel <hugodan4747@gmail.com>
2018-11-18 00:41:55 -0500

    update

--- ECTemperatureSensor.py
+++ ECTemperatureSensor.py
@@ -72,94 +72,95 @@ def getAnalogInput():
     return analog_voltage
 
 
-StartConvert = 0
-ReadTemperature = 1
-
-numReadings = 20            #the number of sample times
-#ECsensorPin = A1            #EC Meter analog output,pin on analog 1
-DS18B20_Pin = 2             #DS18B20 signal, pin on digital 2
-AnalogSampleInterval = 25
-printInterval = 700
-tempSampleInterval = 850    #analog sample interval;serial print interval;temperature sample interval
-readings = []       #3the readings from the analog input
-AnalogAverage = 0
-averageVoltage = 0          #the average
-AnalogSampleTime = 0
-printTime = 0
-
-#float temperature,ECcurrent; 
  
 #Temperature chip i/o
 #OneWire ds(DS18B20_Pin)     #on digital pin 2
 
-#initialize all the readings to 0:
-for thisReading in range(numReadings):
-    readings[thisReading] = 0
-#TempProcess(StartConvert)   #let the DS18B20 start the convert
-tempSampleTime = time.time()
 
-AnalogSampleTime = time.time()
 
-def loop(AnalogSampleTime):
-  
-    #Every once in a while,sample the analog value and calculate the average.
-      
+def loop():
+    
+    numReadings = 20            #the number of sample times
+    #ECsensorPin = A1            #EC Meter analog output,pin on analog 1
+    #DS18B20_Pin = 2             #DS18B20 signal, pin on digital 2
+    AnalogSampleInterval = 25
+    printInterval = 700
+    tempSampleInterval = 850    #analog sample interval;serial print interval;temperature sample interval
+    readings = []       #3the readings from the analog input
+    AnalogAverage = 0
+    averageVoltage = 0          #the average
+    AnalogSampleTime = 0
+    printTime = 0
+    
+    #initialize all the readings to 0:
+    for thisReading in range(numReadings):
+        readings[thisReading] = 0
+    #TempProcess(StartConvert)   #let the DS18B20 start the convert
+    tempSampleTime = time.time()
+    
+    
+    AnalogSampleTime = time.time()
     AnalogValueTotal = 0        #the running total
     index = 0                   #the index of the current reading
     tempSampleTime = 0
     printTime = time.time()
     
-    if(time.time() - AnalogSampleTime) >= AnalogSampleInterval:
-        AnalogSampleTime = time.time()
-         # subtract the last reading:
-        AnalogValueTotal = AnalogValueTotal - readings[index]
-        # read from the sensor:
-        readings[index] = getAnalogInput
-        # add the reading to the total:
-        AnalogValueTotal = AnalogValueTotal + readings[index]
-        # advance to the next position in the array:
-        index = index + 1
-        # if we're at the end of the array...
-        if (index >= numReadings):
-            # ...wrap around to the beginning:
-            index = 0
-        # calculate the average:
-        AnalogAverage = AnalogValueTotal / numReadings
-        
-    """
-     Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
-     Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
-    """
-    if (time.time() - tempSampleTime) >= tempSampleInterval:  
-         tempSampleTime = time.time()
-         temperature = read_temp()  # read the current temperature from the  DS18B20
-
-        
-    """
-     Every once in a while,print the information on the serial monitor.
-    """
-    if (time.time() - printTime) >= printInterval:
-        printTime = time.time()
-        averageVoltage = AnalogAverage * 5000/1024
-        print("Analog value: " + AnalogAverage) #analog average,from 0 to 1023      
-        print("Voltage: " + averageVoltage + "mV") #millivolt average,from 0mv to 4995mV
-        print("temp: " + temperature + "°C")  #current temperature 
-        
-        TempCoefficient = 1.0 + 0.0185 * (temperature - 25.0)    #temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
-        CoefficientVolatge = averageVoltage/TempCoefficient   
-        if CoefficientVolatge < 150:
-            print("No solution!")   #25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
-        elif CoefficientVolatge > 3300:
-            print("Out of the range!")  #>20ms/cm,out of the range
-        else: 
-            if CoefficientVolatge <= 448:
-                ECcurrent = 6.84 * CoefficientVolatge - 64.32   #1ms/cm<EC<=3ms/cm
-            elif CoefficientVolatge <= 1457:
-                ECcurrent = 6.98 * CoefficientVolatge - 127  #3ms/cm<EC<=10ms/cm
-            else:
-                ECcurrent = 5.3 * CoefficientVolatge + 2278    #10ms/cm<EC<20ms/cm
-            ECcurrent = ECcurrent/1000    #convert us/cm to ms/cm
-            print("EC: " + ECcurrent + "ms/cm")  #two decimal
+    while True:        
+        #Every once in a while,sample the analog value and calculate the average.
+                  
+        if(time.time() - AnalogSampleTime) >= AnalogSampleInterval:
+            AnalogSampleTime = time.time()
+             # subtract the last reading:
+            AnalogValueTotal = AnalogValueTotal - readings[index]
+            # read from the sensor:
+            readings[index] = getAnalogInput
+            # add the reading to the total:
+            AnalogValueTotal = AnalogValueTotal + readings[index]
+            # advance to the next position in the array:
+            index = index + 1
+            # if we're at the end of the array...
+            if (index >= numReadings):
+                # ...wrap around to the beginning:
+                index = 0
+            # calculate the average:
+            AnalogAverage = AnalogValueTotal / numReadings
+            
+        """
+         Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
+         Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
+        """
+        if (time.time() - tempSampleTime) >= tempSampleInterval:  
+             tempSampleTime = time.time()
+             temperature = read_temp()  # read the current temperature from the  DS18B20
+    
+            
+        """
+         Every once in a while,print the information on the serial monitor.
+        """
+        if (time.time() - printTime) >= printInterval:
+            printTime = time.time()
+            averageVoltage = AnalogAverage * 5000/1024
+            print("Analog value: " + AnalogAverage) #analog average,from 0 to 1023      
+            print("Voltage: " + averageVoltage + "mV") #millivolt average,from 0mv to 4995mV
+            print("temp: " + temperature + "°C")  #current temperature 
+            
+            TempCoefficient = 1.0 + 0.0185 * (temperature - 25.0)    #temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+            CoefficientVolatge = averageVoltage/TempCoefficient   
+            if CoefficientVolatge < 150:
+                print("No solution!")   #25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+            elif CoefficientVolatge > 3300:
+                print("Out of the range!")  #>20ms/cm,out of the range
+            else: 
+                if CoefficientVolatge <= 448:
+                    ECcurrent = 6.84 * CoefficientVolatge - 64.32   #1ms/cm<EC<=3ms/cm
+                elif CoefficientVolatge <= 1457:
+                    ECcurrent = 6.98 * CoefficientVolatge - 127  #3ms/cm<EC<=10ms/cm
+                else:
+                    ECcurrent = 5.3 * CoefficientVolatge + 2278    #10ms/cm<EC<20ms/cm
+                ECcurrent = ECcurrent/1000    #convert us/cm to ms/cm
+                print("EC: " + ECcurrent + "ms/cm")  #two decimal
+                
+        sleep(1)
 
 """
 ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
